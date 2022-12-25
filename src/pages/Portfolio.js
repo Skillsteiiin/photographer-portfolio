9cd9cf552795a659dea9440b1fa42dd663a4632e
Skillsteiiin <Kolodkina.t2004@mail.ru>
2022-12-25 21:07:26 +0700

    second commit

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,76 @@
+import React, { useContext } from 'react';
+// images
+import Image1 from '../img/portfolio/1.png';
+import Image2 from '../img/portfolio/2.png';
+import Image3 from '../img/portfolio/3.png';
+import Image4 from '../img/portfolio/4.png';
+// Link
+import { Link } from 'react-router-dom';
+// motion
+import { motion } from 'framer-motion';
+// transition
+import { transition1 } from '../transitions'
+// context
+import { CursorContext } from '../context/CursorContext';
+
+
+const Portfolio = () => {
+const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
+  return <motion.section
+  initial={{ opacity: 0, y: '100%' }}
+  animate={{ opacity: 1, y: 0 }}
+  exit={{ opacity: 0, y: '100%' }}
+  transition={ transition1 }
+  className='section'
+  >
+
+    <div    
+       className='container mx-auto h-full relative'>
+      <div
+      className='flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8'>
+          {/* { text } */}
+          <motion.div
+            initial={{ opacity: 0, y: '-80%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-80%' }}
+            transition={ transition1 }
+            onMouseEnter={mouseEnterHandler}
+            onMouseLeave={mouseLeaveHandler}
+            className='flex flex-col lg:items-start'
+            >
+            <h1 className='h1'>Portfolio</h1>
+            <p className='mb-12 max-w-sm'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            <b>Lorem Ipsum</b> has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+          </p>
+          <Link to={'/contact'} className='btn mb-[30px] mx-auto lg:mx-0'>
+            Hire me
+          </Link>
+        </motion.div>
+          {/* { image grid } */}
+          <div
+           className='grid grid-cols-2 lg:gap-2'>
+            {/* { image } */}
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500' src={Image1} alt='img1' />
+            </div>
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500' src={Image2} alt='img1' />
+            </div>
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500' src={Image3} alt='img1' />
+            </div>
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500' src={Image4} alt='img1' />
+            </div>
+          </div>
+        </div>
+    </div>
+  </motion.section>;
+};
+
+export default Portfolio;
